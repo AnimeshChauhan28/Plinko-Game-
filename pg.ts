53fdbf7bf040c357f4c8d51bcd036e06c2800e74
AnimeshChauhan28 <animeshchauhan28@gmail.com>
2024-08-11 02:22:58 +0530

    Create pg.ts

---
+++ pg.ts
@@ -0,0 +1,18 @@
+import "./App.css"
+import {BrowseRouter,Routes,Route} from "react-router-dom";
+import {Simulation } from './components/Simulation';
+import {Game} from './components/Game';
+
+function App(){
+    return (
+        <>
+         <BrowserRouter>
+          <Routes>
+            <Route path="simulation" element={< Simulation />} />
+            <Route path ="game" element={<Game />} />
+          </Routes>
+         </BrowserRouter>
+        </>   
+    )
+}
+export default App
